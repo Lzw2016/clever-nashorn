d2627fc8a894088fde7f340f59bbd5db58397286
lizw <lzw1000000@163.com>
2019-08-26 11:31:21 +0800

    test

--- clever-nashorn-commonjs/src/test/resources/tmp/t3.js
+++ clever-nashorn-commonjs/src/test/resources/tmp/t3.js
@@ -10,8 +10,8 @@ exports.printJS = function () {
     console.log("打印JS变量 | boolean {}", true);
     console.log("打印JS变量 | string {}", "nashorn");
     console.log("打印JS变量 | date {}", new Date());
-    console.log("打印JS变量 | array {}", [1, 2.2, true, "nashorn", null, new Date()]);
-    console.log("打印JS变量 | object {}", {"string": "aaa", "number1": 12, "number2": 12.345, "boolean": true, "null": null, "date": new Date()});
+    console.log("打印JS变量 | array {}", [1, 2.2, true, "nashorn", null, new Date(), function () { return 1 + 6; }]);
+    console.log("打印JS变量 | object {}", {"string": "aaa", "number1": 12, "number2": 12.345, "boolean": true, "null": null, "date": new Date(), "function": function () { return 1 + 6; }});
     console.log("打印JS变量 | function {}", function () { return 1 + 6; });
     console.log("打印JS变量 | JSON.stringify {}", JSON.stringify({date: new Date()}));
 };
