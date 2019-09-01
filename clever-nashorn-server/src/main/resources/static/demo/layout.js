cbadb152a3bb59c729fa76339e6e77e522ccf87c
Lzw2016 <lizhiwei1993>
2019-09-01 23:28:03 +0800

    优化

--- clever-nashorn-server/src/main/resources/static/demo/layout.js
+++ clever-nashorn-server/src/main/resources/static/demo/layout.js
@@ -1,9 +1,10 @@
 // noinspection ES6ConvertVarToLetConst
 var layoutRoot = Split(['.layout-left', '.layout-right'], {
     direction: 'horizontal',
-    sizes: [15, 85],
+    sizes: [10, 90],
     // sizes: ['296px', 'calc(100% - 300px)'],
-    minSize: 300,
+    // sizes: ['296px'],
+    minSize: [210, 800],
     gutterSize: 4,
     onDragEnd: function () {
         if (editor) {
@@ -17,7 +18,7 @@ var layoutCenter = Split(['.layout-top', '.layout-bottom'], {
     direction: 'vertical',
     sizes: [70, 30],
     // sizes: ['calc(100% - 360px)', '356px'],
-    minSize: 260,
+    minSize: 160,
     gutterSize: 4,
     onDragEnd: function () {
         if (editor) {
