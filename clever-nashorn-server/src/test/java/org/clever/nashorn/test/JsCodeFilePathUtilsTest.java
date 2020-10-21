84148e1ffcd38dedf3d37c580d57e0fccd85301c
lizw <lzw1000000@163.com>
2020-10-21 09:06:56 +0800

    修改数据库 b2b-prod-ro 用户名密码

--- clever-nashorn-server/src/test/java/org/clever/nashorn/test/JsCodeFilePathUtilsTest.java
+++ clever-nashorn-server/src/test/java/org/clever/nashorn/test/JsCodeFilePathUtilsTest.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.clever.nashorn.utils.JsCodeFilePathUtils;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * 作者：lizw <br/>
  * 创建时间：2019/08/27 17:42 <br/>
@@ -25,5 +27,15 @@ public class JsCodeFilePathUtilsTest {
         String suffix = ".json";
         requestUri = requestUri.substring(0, requestUri.length() - suffix.length());
         log.info("{}", requestUri);
+    }
+
+    @Test
+    public void t03() {
+        final String requestUri = "/aaa/bbb/ccc/ddd/eee/fff1@mmm.json";
+        String tmp = requestUri.substring(requestUri.lastIndexOf("/"));
+        String[] arr = tmp.split("@");
+        log.info("{}", Arrays.toString(arr));
+
+        log.info("{}", requestUri.substring(0, requestUri.length() - (arr[1].length() + 1)));
     }
 }
